dc45fcf5417625d31e042bf0f43cddd7807230f3
Diego Sousa <dmenezessousaa@gmail.com>
2021-05-26 16:14:18 -0400

    Create adder.js

---
+++ adder.js
@@ -0,0 +1,10 @@
+// add numbers, then log it
+const num1 = 5
+const num2 = 10
+
+console.log("5 + 10 =", num1 + num2);
+
+// get number + result, then log it 
+const addResult = 15
+
+console.log("5 and 10 add to 15 =", num1 + num2 + addResult);
