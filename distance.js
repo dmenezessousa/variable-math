e9e1149f3c7bb06de466ed88542112f4603ffa1e
Diego Sousa <dmenezessousaa@gmail.com>
2021-05-26 16:15:20 -0400

    Create distance.js

---
+++ distance.js
@@ -0,0 +1,9 @@
+// distance vvariables
+const x1 = 5
+const y1 = 10
+const x2 = 15
+const y2 = 20
+
+// distance between coordinates
+
+console.log("The distance between Point (x1.y1) and point (x2, y2) is =", Math.sqrt((x2 - x1) *2 + (y2 - y1) *2));
