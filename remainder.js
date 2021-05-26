fc5fa6cffe4346cd442222f773d947f38848e9e3
Diego Sousa <dmenezessousaa@gmail.com>
2021-05-26 16:14:57 -0400

    Create remainder.js

---
+++ remainder.js
@@ -0,0 +1,7 @@
+// positive numbers
+const numPositive1 = 7
+const numPositive2 = 3
+
+// get possitives number, get remainders,then log it
+
+console.log("The remainder of 7 divided by 3 is =", numPositive1 % numPositive2); 
