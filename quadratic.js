3ee1bf56fcd8835ddad97689d2132289ea9524a2
Diego Sousa <dmenezessousaa@gmail.com>
2021-05-26 16:15:40 -0400

    Create quadratic.js

---
+++ quadratic.js
@@ -0,0 +1,7 @@
+// get quad, then log it
+const a = 1
+const b = 12
+const c = 3
+
+console.log(-b + Math.sqrt(b * 2 )- (4 * a * c) / (2 * a));
+console.log(-b - Math.sqrt(b * 2 )- (4 * a * c) / (2 * a));
